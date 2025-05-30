0104d136aee8ee871d75f68f0757f568ee75bae0
Sahan <kksahan78@gmail.com>
2025-05-30 18:04:34 +0530

    Initial Commit

---
+++ src/main/java/com/buzznote/notification/BuzzNoteNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.buzznote.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuzzNoteNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuzzNoteNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buzznote/notification/buzznote_notification_service/BuzznoteNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buzznote.notification.buzznote_notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuzznoteNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
